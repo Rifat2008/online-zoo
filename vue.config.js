b61822517c3f527bf4afbc3c9a1cf27c699f5bd6
Rifat <rifatkhabibulin88@gmail.comgit config --global user.name Rifatgit statusgit config user.email rifatkhabibulin88@gmail.com>
2023-02-24 18:46:58 +0300

    fix: update publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/online-zoo/',
+  publicPath: '/main',
   transpileDependencies: true,
   lintOnSave: false,
   css: {
